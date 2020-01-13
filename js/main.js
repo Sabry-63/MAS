d3e0ced011a2dd2ab5d03e6de5263d8aad9281cf
Sabry-63 <52372174+Sabry-63@users.noreply.github.com>
2020-01-13 09:39:54 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -16,5 +16,40 @@ $(function () {
     
     $('.mas-vid').height(winH - (upperH + down + navH));
     
+    // Slick Plagen
+    $('.slider').slick({
+        dots: true,
+        infinite: true,
+        autoplay:true,
+        autoplaySpeed: 3000,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 4,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
     
 });
