383c222ed7b1776c2a311257ea32c56c170fb6f5
Sabry-63 <52372174+Sabry-63@users.noreply.github.com>
2020-01-16 13:34:37 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,23 +1,27 @@
 /*global $, console */
 
-$(function () {
-    'use strict';
+$(function() {
+    "use strict";
     // active class nav
-    $('.navbar-nav .nav-link').click(function () {
-        $(this).parent('.nav-item').siblings().children().removeClass('active');
-        $(this).addClass('active');
+    $(".navbar-nav .nav-link").click(function() {
+        $(this)
+            .parent(".nav-item")
+            .siblings()
+            .children()
+            .removeClass("active");
+        $(this).addClass("active");
     });
-    
+
     // hieght video header
-    var winH     = $(window).height(),
-        upperH   = $('.upper-nav').innerHeight(),
-        down     = $('.info-list').innerHeight(),
-        navH     = $('.navbar').innerHeight();
-    
-    $('.mas-vid').height(winH - (upperH + down + navH));
-    
+    var winH = $(window).height(),
+        upperH = $(".upper-nav").innerHeight(),
+        down = $(".info-list").innerHeight(),
+        navH = $(".navbar").innerHeight();
+
+    $(".mas-vid").height(winH - (upperH + down + navH));
+
     // Slick Plagen
-    $('.slider').slick({
+    $(".slider").slick({
         dots: false,
         arrows: false,
         autoplay: true,
@@ -26,28 +30,34 @@ $(function () {
         slidesToShow: 3,
         slidesToScroll: 1,
         responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 1,
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
             }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
         ]
-      });
-    
+    });
+
+    /* calling script */
+    $(".xzoom, .xzoom-gallery").xzoom({
+        tint: "#333",
+        Xoffset: 15 ,
+        openOnSmall: true
+    });
 });
